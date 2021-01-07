51cf20596ba1648a4f4373f79b617de0c3e667ce
bassidus <basse1978@gmail.com>
2021-01-07 14:20:13 +0100

    part five

--- main.js
+++ main.js
@@ -8,6 +8,31 @@ var app = new Vue({
     inventory: 100,
     onSale: true,
     details: ["80% cotton", "20% polyester", "Gender-neutral"],
-    sizes: ["XXS, ", "XS, ", "S, ", "M, ", "L, ", "XL, ", "XXL"]
+    sizes: ["XXS, ", "XS, ", "S, ", "M, ", "L, ", "XL, ", "XXL"],
+    cart: 0,
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green",
+        variantImage: "./assets/vmSocks-green-onWhite.jpg",
+      },
+      {
+        variantId: 3244,
+        variantColor: "blue",
+        variantImage: "./assets/vmSocks-blue-onWhite.png",
+      },
+    ],
+  },
+  methods: {
+    addToCart() {
+      this.cart++;
+    },
+    removeFromCart() {
+        this.cart--;
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage;
+    },
+    
   },
 });
