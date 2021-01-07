fc9ca0fcb822402467a36cf9732343cd4d58aa69
bassidus <basse1978@gmail.com>
2021-01-07 13:47:51 +0100

    part four

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-// var product = "Socks";
 var app = new Vue({
-    el: '#app',
-    data: {
-        product: "Socks",
-        altText: "A pair of warm, fuzzy socks",
-        image: "./assets/vmSocks-green-onWhite.jpg",
-        url: "http://oklart.mine.nu",
-        inventory: 100,
-        onSale: true,
-    }
-})
-
+  el: "#app",
+  data: {
+    product: "Socks",
+    altText: "A pair of warm, fuzzy socks",
+    image: "./assets/vmSocks-green-onWhite.jpg",
+    url: "http://oklart.mine.nu",
+    inventory: 100,
+    onSale: true,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    sizes: ["XXS, ", "XS, ", "S, ", "M, ", "L, ", "XL, ", "XXL"]
+  },
+});
